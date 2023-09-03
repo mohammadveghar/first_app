d080f3d4687c65bd33415e8967ad9a470cc6f64c
Mohammad Vaghar <mohammad.veghar.92@gmail.com>
2023-09-04 01:31:29 +0330

    I created my first project that is a store like Digikala

---
+++ src/Car.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+class Car extends Component {
+   render() {
+      return (
+         <p>
+            the model of car is benz and its color is black
+         </p>
+      )
+   }
+}
+export default Car 
