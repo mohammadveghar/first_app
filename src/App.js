3cf6c00bfa1c94f23bf4cd7268c45422579358c9
Mohammad Vaghar <mohammad.veghar.92@gmail.com>
2023-09-04 14:53:36 +0330

    deleting excess files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>
-        react                      v18
-      </h1>
+    <div>
+  
     </div>
   );
 }
