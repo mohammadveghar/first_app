cafdd1c766e7acbc03480019d3b9f97d91c6d402
Mohammad Vaghar <mohammad.veghar.92@gmail.com>
2023-09-16 22:46:44 +0330

    counter added to Card component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,15 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import styles from "./Search.module.css";
 //.........................................................
 class Search extends Component {
-   render() {
-      return (
-         <div className={styles.container}>
-            <p>Search what you want</p>
-            <input placeholder="Search..."/>
-         </div>
-      );
+	constructor() {
+		super();
+		this.state = {
+			text: "",
+		};
    }
+   changeHandler = (event) => {
+      this.setState({
+         text : event.target.value
+      })
+   }
+	render() {
+		return (
+			<div className={styles.container}>
+            <p>Search what you want</p>
+            <div>
+            <input  type="text" value= {this.state.text} onChange ={this.changeHandler} placeholder="Search..." />
+            <br/>
+            <br/>
+            <br/>
+               <span>{this.state.text}</span>
+            </div>
+            
+			</div>
+		);
+	}
 }
 
 export default Search;
